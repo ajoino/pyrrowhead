d74e80f47e659df4500db88661fd8362c9c959a1
ajoino <jacob.nilsson@ltu.se>
2022-03-09 10:08:16 +0100

    Removed unused test files.

--- tests/cli_test_commands.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from typer.testing import CliRunner
-from pyrrowhead.main import app
-
-
-def run_cloud_setup(cloud_identifier: str):
-    return CliRunner().invoke(app, ["cloud", "setup", cloud_identifier])
